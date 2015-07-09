ef65191180c3022072eead2f802a9f80454f07da
Sashko Stubailo <sashko@stubailo.com>
2015-07-08 20:58:35 -0700

    Add content from website

---
+++ routes/angularTut.js
@@ -0,0 +1,108 @@
+ANGULAR_TUT = [
+  {
+    id: "0",
+    title: 'Creating an app',
+    seoTitle: 'Creating an app',
+    route: "tutorials.angular.creating-an-app",
+    path: "/tutorials/angular/creating-an-app",
+    contentTemplate: 'angular-step01',
+    sha: "fc766519b453d3d57820a6fe8839dae173f49941"
+  },
+  {
+    id: "1",
+    title: 'Templates',
+    seoTitle: 'Templates',
+    route: "tutorials.angular.templates",
+    path: "/tutorials/angular/templates",
+    contentTemplate: 'angular-step02',
+    sha: "7eea17a2de2c45031b7f345e4db7ec1152d6386f"
+  },
+  {
+    id: "2",
+    title: 'Collections',
+    seoTitle: 'Collections',
+    route: "tutorials.angular.collections",
+    path: "/tutorials/angular/collections",
+    contentTemplate: 'angular-step03',
+    sha: "8bc939ece149a547e1130c056853b13f89519627"
+  },
+  {
+    id: "3",
+    title: 'Forms and events',
+    seoTitle: 'Forms and events',
+    route: "tutorials.angular.forms-and-events",
+    path: "/tutorials/angular/forms-and-events",
+    contentTemplate: 'angular-step04',
+    sha: "785d5fa76b32ba37e2a73bccea2cebfe27b79c94"
+  },
+  {
+    id: "4",
+    title: 'Update and remove',
+    seoTitle: 'Update and remove',
+    route: "tutorials.angular.update-and-remove",
+    path: "/tutorials/angular/update-and-remove",
+    contentTemplate: 'angular-step05',
+    sha: "2c5420e51ca2d0177d83befad0730adea8e75c4a"
+  },
+  {
+    id: "5",
+    title: 'Deploying your app',
+    seoTitle: 'Deploying your app',
+    route: "tutorials.angular.deploying-your-app",
+    path: "/tutorials/angular/deploying-your-app",
+    contentTemplate: 'angular-step06'
+  },
+  {
+    id: "6",
+    title: 'Running on mobile',
+    seoTitle: 'Running on mobile',
+    route: "tutorials.angular.running-on-mobile",
+    path: "/tutorials/angular/running-on-mobile",
+    contentTemplate: 'angular-step07',
+    sha: "f285fe5d508411b1aed368a818263eb23239f024"
+  },
+  {
+    id: "7",
+    title: 'Filtering Collections',
+    seoTitle: 'Filtering Collections',
+    route: "tutorials.angular.filtering-collections",
+    path: "/tutorials/angular/filtering-collections",
+    contentTemplate: 'angular-step08',
+    sha: "f11a875b19ccf321e62fd5494d8de5d03df0c55e"
+  },
+  {
+    id: "8",
+    title: 'Adding user accounts',
+    seoTitle: 'Adding user accounts',
+    route: "tutorials.angular.adding-user-accounts",
+    path: "/tutorials/angular/adding-user-accounts",
+    contentTemplate: 'angular-step09',
+    sha: "85b6b6606e8a0742250614041455784d8455810c"
+  },
+  {
+    id: "9",
+    title: 'Security with methods',
+    seoTitle: 'Security with methods',
+    route: "tutorials.angular.security-with-methods",
+    path: "/tutorials/angular/security-with-methods",
+    contentTemplate: 'angular-step10',
+    sha: "75b8328206dc9a6a0f8ce019da35446c63b5b5d1"
+  },
+  {
+    id: "10",
+    title: 'Publish and subscribe',
+    seoTitle: 'Publish and subscribe',
+    route: "tutorials.angular.publish-and-subscribe",
+    path: "/tutorials/angular/publish-and-subscribe",
+    contentTemplate: 'angular-step11',
+    sha: "a85b2f031fa08fb1ae09bdc69d55ce760d44bf72"
+  },
+  {
+    id: "11",
+    title: 'Next steps',
+    seoTitle: 'Next steps',
+    route: "tutorials.angular.next-steps",
+    path: "/tutorials/angular/next-steps",
+    contentTemplate: 'angular-step12'
+  }
+];
---
+++ routes/blazeTut.js
@@ -0,0 +1,107 @@
+BLAZE_TUT = [
+  {
+    id: "0",
+    title: 'Creating an app',
+    seoTitle: 'Creating an app',
+    route: "tutorials.blaze.creating-an-app",
+    path: "/tutorials/blaze/creating-an-app",
+    contentTemplate: 'blaze-step01',
+    sha: "377a8610c2fa77056d015e6998d5eb894436c99e"
+  },
+  {
+    id: "1",
+    title: 'Templates',
+    seoTitle: 'Templates',
+    route: "tutorials.blaze.templates",
+    path: "/tutorials/blaze/templates",
+    contentTemplate: 'blaze-step02',
+    sha: "9f5c65302f1527f25cacaaf275ea5c747d55e405"
+  },
+  {
+    id: "2",
+    title: 'Collections',
+    seoTitle: 'Collections',
+    route: "tutorials.blaze.collections",
+    path: "/tutorials/blaze/collections",
+    contentTemplate: 'blaze-step03',
+    sha: "ab14d317db174a3b2d408f4e24a50c7af13342cd"
+  },
+  {
+    id: "3",
+    title: 'Forms and events',
+    seoTitle: 'Forms and events',
+    route: "tutorials.blaze.forms-and-events",
+    path: "/tutorials/blaze/forms-and-events",
+    contentTemplate: 'blaze-step04',
+    sha: "9c24b998e540848f0dbc241702a4fcfa48fb9087"
+  },
+  {
+    id: "4",
+    title: 'Update and remove',
+    seoTitle: 'Update and remove',
+    route: "tutorials.blaze.update-and-remove",
+    path: "/tutorials/blaze/update-and-remove",
+    contentTemplate: 'blaze-step05',
+    sha: "4e6f9494c895eff43fe26167a7b803a24c35854e"
+  },
+  {
+    id: "5",
+    title: 'Deploying your app',
+    seoTitle: 'Deploying your app',
+    route: "tutorials.blaze.deploying-your-app",
+    path: "/tutorials/blaze/deploying-your-app",
+    contentTemplate: 'blaze-step06'
+  },
+  {
+    id: "6",
+    title: 'Running on mobile',
+    seoTitle: 'Running on mobile',
+    route: "tutorials.blaze.running-on-mobile",
+    path: "/tutorials/blaze/running-on-mobile",
+    contentTemplate: 'blaze-step07'
+  },
+  {
+    id: "7",
+    title: 'Temporary UI state',
+    seoTitle: 'Temporary UI state',
+    route: "tutorials.blaze.temporary-ui-state",
+    path: "/tutorials/blaze/temporary-ui-state",
+    contentTemplate: 'blaze-step08',
+    sha: "ad35c7e02a9f7c40f45f40dbff8d297fba4b13ac"
+  },
+  {
+    id: "8",
+    title: 'Adding user accounts',
+    seoTitle: 'Adding user accounts',
+    route: "tutorials.blaze.adding-user-accounts",
+    path: "/tutorials/blaze/adding-user-accounts",
+    contentTemplate: 'blaze-step09',
+    sha: "bb4a0fc190c44aa8cfd4189a03c2e18f1294074c"
+  },
+  {
+    id: "9",
+    title: 'Security with methods',
+    seoTitle: 'Security with methods',
+    route: "tutorials.blaze.security-with-methods",
+    path: "/tutorials/blaze/security-with-methods",
+    contentTemplate: 'blaze-step10',
+    sha: "e3b7fa8ed373c9e6b96fb3c370d0af238170b89a"
+  },
+  {
+    id: "10",
+    title: 'Publish and subscribe',
+    seoTitle: 'Publish and subscribe',
+    route: "tutorials.blaze.publish-and-subscribe",
+    path: "/tutorials/blaze/publish-and-subscribe",
+    contentTemplate: 'blaze-step11',
+    sha: "44dfd82a9e90cf8600c18b7e9f3c2e742c34285d"
+  },
+  {
+    id: "11",
+    title: 'Next steps',
+    seoTitle: 'Next steps',
+    route: "tutorials.blaze.next-steps",
+    path: "/tutorials/blaze/next-steps",
+    contentTemplate: 'blaze-step12'
+  }
+];
