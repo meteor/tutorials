ef11924a22d30d5c03eca779023a636f49e8c8a7
Jesse Rosenberger <git@jro.cc>
2017-03-21 11:23:03 +0200

    Set `versionsFrom` to a non-`rc.*` version of Meteor.

--- package.js
+++ package.js
@@ -5,7 +5,7 @@ Package.describe({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.2-rc.14');
+  api.versionsFrom('1.2.1');
   api.use([
     'simple:markdown-templating@1.2.7',
     'templating',
